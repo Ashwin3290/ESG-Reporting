ef8e22a25291669c880a3b9197959fad794c221f
Ashwin Choubey <ashwinchoubey03092002@gmail.com>
2024-11-18 01:10:41 +0530

    testing some things

--- test.py
+++ test.py
@@ -3,3 +3,9 @@ import pandas as pd
 df=pd.read_csv("data/kpi_data.csv")
 print(df.head())
 print(df.info())
+
+from collections import Counter
+
+kpi_words = df['KPI Name'].str.split().explode().str.lower()
+common_words = Counter(kpi_words).most_common(50)  # Display top 50 most common words
+common_words
