a9902004c49d33496944a967291d4f763b6681e8
Ashwin Choubey <ashwinchoubey03092002@gmail.com>
2024-11-18 01:26:04 +0530

    classified kpis

--- test.py
+++ test.py
@@ -7,5 +7,6 @@ print(df.info())
 from collections import Counter
 
 kpi_words = df['KPI Name'].str.split().explode().str.lower()
-common_words = Counter(kpi_words).most_common(50)  # Display top 50 most common words
-common_words
+common_words = Counter(kpi_words).most_common(75)  # Display top 50 most common words
+
+print(common_words)
